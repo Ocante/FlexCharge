ac286f19ed2a2ce67599f866fb4b336a42aef2fa
Ocante <ocanteantonio91@gmail.com>
2025-12-07 00:37:17 -0300

    "Corrige spinner e fragments; ajustes UI

--- app/src/main/java/com/bless/flexcharge/MainActivity.kt
+++ app/src/main/java/com/bless/flexcharge/MainActivity.kt
@@ -1,20 +1,60 @@
 package com.bless.flexcharge
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.AdapterView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.bless.flexcharge.databinding.ActivityMainBinding
+import com.bless.flexcharge.fuel.FuelFragment
+import com.bless.flexcharge.electric.ElectricFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val options = listOf("Selecione...", "Combustível", "Elétrico")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, options).also {
+            it.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        }
+        binding.spinnerType.adapter = adapter
+
+        // garante que não há fragmento carregado no começo
+        clearFragment()
+
+        binding.spinnerType.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                when (position) {
+                    1 -> loadFragment(FuelFragment())
+                    2 -> loadFragment(ElectricFragment())
+                    else -> clearFragment()
+                }
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                clearFragment()
+            }
+        }
+    }
+
+    // Usa API clássica (sem KTX) para evitar erro de "unresolved reference commit"
+    private fun loadFragment(fragment: androidx.fragment.app.Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        // se quiser animação, adicione aqui transaction.setCustomAnimations(...)
+        transaction.replace(R.id.fragmentContainer, fragment)
+        transaction.commit()
+    }
+
+    private fun clearFragment() {
+        val frag = supportFragmentManager.findFragmentById(R.id.fragmentContainer)
+        if (frag != null) {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.remove(frag)
+            transaction.commit()
         }
     }
 }
